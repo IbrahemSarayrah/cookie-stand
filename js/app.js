3d06399d00cc3d2e996525f324b4412ff111a495
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-05-03 02:14:25 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-let workHour =['6am', '7am' ,'8am' ,'9am' ,'10am', '11am', '12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am',];
 
 function randomCoustomer(min , max){
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -8,6 +7,7 @@ function randomCoustomer(min , max){
 
 let Seattle = {
   location : 'Seattle',
+  workHour : ['6am', '7am' ,'8am' ,'9am' ,'10am', '11am', '12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am',],
   minCustomer : 23,
   maxCustomer : 65,
   avgCookieSales : 6.3,
@@ -15,15 +15,15 @@ let Seattle = {
   avgCookiePerCoustomer :[],
 
   getHourlyCustomer: function () {
-    for (let i = 0 ; i < workHour.length ; i++) {
+    for (let i = 0 ; i < this.workHour.length ; i++) {
       let random = randomCoustomer(this.minCustomer , this.maxCustomer);
       this.hourlyCustomer.push(random);
       console.log(this.hourlyCustomer);
     }
   },
   getAvgCoookiePerCustomer: function(){
-    for (let i=0; i<workHour.length; i++){
-      let randomCookie = this.hourlyCustomer[i] *this.avgCookiePerCoustomer;
+    for (let i=0; i<this.hourlyCustomer.length; i++){
+      let randomCookie = Math.floor(this.hourlyCustomer[i] * this.avgCookieSales);
       this.avgCookiePerCoustomer.push(randomCookie);
       console.log(this.avgCookiePerCoustomer);
     }
