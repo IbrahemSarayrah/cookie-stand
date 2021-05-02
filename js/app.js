46932ce2a4efb465a20329025983b011f59a6b1f
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-05-03 01:58:06 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
+let workHour =['6am', '7am' ,'8am' ,'9am' ,'10am', '11am', '12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am',];
+
+function randomCoustomer(min , max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 let Seattle = {
   location : 'Seattle',
   minCustomer : 23,
@@ -7,19 +13,26 @@ let Seattle = {
   avgCookieSales : 6.3,
   hourlyCustomer: [],
   avgCookiePerCoustomer :[],
-  workHour : ['6am', '7am' ,'8am' ,'9am' ,'10am', '11am', '12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am',],
-  randomCoustomer: function(){
-    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
-  },
-  getHourlyCustomer: function () {
-    for (let i = 0 ; i < this.workHour ; i++) {
-   
-    }
 
+  getHourlyCustomer: function () {
+    for (let i = 0 ; i < workHour.length ; i++) {
+      let random = randomCoustomer(this.minCustomer , this.maxCustomer);
+      this.hourlyCustomer.push(random);
+      console.log(this.hourlyCustomer);
+    }
+  },
+  getAvgCoookiePerCustomer: function(){
+    for (let i=0; i<workHour.length; i++){
+      let randomCookie = this.hourlyCustomer[i] *this.avgCookiePerCoustomer;
+      this.avgCookiePerCoustomer.push(randomCookie);
+      console.log(this.avgCookiePerCoustomer);
+    }
   }
 };
 
-
-console.log(Seattle.hourlyCustomer);
-console.log(Seattle.randomCoustomer());
+Seattle.getHourlyCustomer();
+Seattle.getAvgCoookiePerCustomer();
+//console.log(randomCoustomer(25,65));
+// console.log(Seattle.hourlyCustomer);
+// console.log(Seattle.randomCoustomer());
 
